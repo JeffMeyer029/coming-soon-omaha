121b5379df3fcab9bd868548a0c825ea804ffd70
Jeffrey Meyer <jeffreymeyer@Mac.lan>
2025-08-26 13:25:22 -0400

    Add PostHog tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,6 +39,21 @@ export default function RootLayout({
       `,
     }}
   />
+  <script
+  dangerouslySetInnerHTML={{
+    __html: `
+      !function(t,e){var o,n,p,r;e.__SV||(window.posthog=e,e._i=[],e.init=function(i,s,a){
+      function g(t,e){var o=e.split(".");2==o.length&&(t=t[o[0]],e=o[1]),t[e]=function(){
+      t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}var u=e;"undefined"!==
+      typeof a?u=e[a]=[]:a="posthog",u.people=u.people||[],u.toString=function(t){
+      var e="posthog";return"posthog"!==a&&(e+="."+a),t||(e+=" (stub)"),e},u.people.toString=
+      function(){return u.toString(1)+".people (stub)"},o="capture identify alias people.set people.set_once set_config register register_once unregister opt_out_capturing has_opted_out_capturing opt_in_capturing reset isFeatureEnabled onFeatureFlags reloadFeatureFlags".split(" ");
+      for(n=0;n<o.length;n++)g(u,o[n]);e._i.push([i,s,a])},e.__SV=1.2,
+      e.init('phc_AqdCjyLV8H79u7HiARctDyahve6fYJM0tFsGMEpkNSW',{api_host:'https://us.i.posthog.com'})}
+      (document,window.posthog||[]);
+    `,
+  }}
+/>
 </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
